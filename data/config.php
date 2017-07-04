491dd078adbf729887db0efdf4c6794067164a83
Xipan Xiao <xxp9@hotmail.com>
2017-07-04 12:26:20 -0700

    change config.php

--- data/config.php
+++ data/config.php
@@ -6,28 +6,12 @@
     "dbname": "buddcourses",
     "zbServiceUrl": "http://www.zhibei.info/zb"
   },
-  "10.254.151.229": {
-    "servername": "127.0.0.1",
-    "username": "root",
-    "password": "0217",
-    "dbname": "buddcourses",
-    "zbServiceUrl": "http://www.zhibei.info/zb"
-  },
-  "buddcourses-bicw.rhcloud.com": {
+  "msy-bicwdev.rhcloud.com": {
     "servername": "${OPENSHIFT_MYSQL_DB_HOST}",
     "username": "${OPENSHIFT_MYSQL_DB_USERNAME}",
     "password": "${OPENSHIFT_MYSQL_DB_PASSWORD}",
     "dbname": "buddcourses",
     "session_path": "${OPENSHIFT_TMP_DIR}",
-    "zbServiceUrl": "http://www.zhibei.info/zb"
-  },
-  "buddcourses-bicwdev.rhcloud.com": {
-    "servername": "${OPENSHIFT_MYSQL_DB_HOST}",
-    "username": "${OPENSHIFT_MYSQL_DB_USERNAME}",
-    "password": "${OPENSHIFT_MYSQL_DB_PASSWORD}",
-    "dbname": "buddcourses",
-    "session_path": "${OPENSHIFT_TMP_DIR}",
-    "zbServiceUrl": ""
   },
   "closing": "*/?>"
 }
