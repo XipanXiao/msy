7a9813407862675765f697dda9ecf53536023598
Xipan Xiao <xxp9@hotmail.com>
2017-07-11 19:25:45 -0700

    move cart to a separate cart.js

---
+++ js/model/cart.js
@@ -0,0 +1,103 @@
+define('model/cart', [], function() {
+  var utils, rpc, rootScope;
+
+  var cart = {
+    size: 0,
+    subTotal: 0.0,
+    shipping: 0.0,
+    items: {},
+    add: function(item) {
+      var existing = this.items[item.id];
+      if (existing) {
+        existing.count++;
+      } else {
+        item.count = 1;
+        this.items[item.id] = item;
+      }
+      this.update();
+    },
+    remove: function(id) {
+      delete this.items[id];
+      this.update();
+    },
+    update: function() {
+      this.size = 0;
+      this.subTotal = 0.0;
+      this.int_shipping = 0.0;
+      this.shipping = 0.0;
+      for (var id in this.items) {
+        var item = this.items[id];
+        this.size += item.count;
+        this.subTotal += item.count * item.price;
+        this.int_shipping += item.count * item.int_shipping;
+        this.shipping += item.count * item.shipping;
+      }
+      this.subTotal = this.subTotal.toFixed(2);
+      this.int_shipping = this.int_shipping.toFixed(2);
+      this.shipping = (this.shipping || 0).toFixed(2);
+    },
+    clear: function() {
+      this.items = {};
+      this.update();
+    },
+    checkOut: function(user) {
+      var order = {
+        user_id: user.id,
+        sub_total: this.subTotal,
+        int_shipping: this.int_shipping,
+        shipping: this.shipping,
+        name: user.name,
+        phone: user.phone,
+        email: user.email,
+        street: user.street,
+        city: user.city,
+        state: user.state,
+        country: user.country,
+        zip: user.zip,
+        items: []
+      };
+      for (var id in this.items) {
+        var item = this.items[id];
+        order.items.push({
+          item_id: item.id,
+          price: item.price,
+          count: item.count
+        });
+      }
+      var cart = this;
+
+      var saveUserInfo = function() {
+        if (user.isNew) {
+          delete user.isNew;
+          delete user.id;
+        }
+        return rpc.update_user(user).then(function(response) {
+          var created = response.data.updated;
+          if (!created) return order.user_id;
+          return order.user_id = created.id;
+        });
+      };
+      
+      var placeOrder = function() {
+        return rpc.update_order(order).then(function(response) {
+          if (parseInt(response.data.updated)) {
+            cart.clear();
+            rootScope.$broadcast('reload-orders');
+            document.querySelector('#toast0').open();
+            return true;
+          }
+          return false;
+        });
+      };
+      
+      return utils.requestOneByOne([saveUserInfo, placeOrder]);
+    }
+  };
+  return function(params) {
+    utils = params.utils;
+    rpc = params.rpc;
+    rootScope = params.rootScope;
+
+    utils.mix_in(this, cart);
+  };
+});
--- js/order_app.js
+++ js/order_app.js
@@ -1,11 +1,12 @@
 define('order_app', [
+    'model/cart',
     'app_bar/app_bar',
     'item_list/item_list',
     'shopping_cart/shopping_cart',
     'services',
     'permission',
     'utils'],
-    function() {
+    function(Cart) {
 
   angular.module('AppModule', [
       'AppBarModule',
@@ -20,98 +21,8 @@ define('order_app', [
           link: function(scope) {
             scope.pageLoaded = [];
             
-            scope.cart = {
-              size: 0,
-              subTotal: 0.0,
-              shipping: 0.0,
-              items: {},
-              add: function(item) {
-                var existing = this.items[item.id];
-                if (existing) {
-                  existing.count++;
-                } else {
-                  item.count = 1;
-                  this.items[item.id] = item;
-                }
-                this.update();
-              },
-              remove: function(id) {
-                delete this.items[id];
-                this.update();
-              },
-              update: function() {
-                this.size = 0;
-                this.subTotal = 0.0;
-                this.int_shipping = 0.0;
-                this.shipping = 0.0;
-                for (var id in this.items) {
-                  var item = this.items[id];
-                  this.size += item.count;
-                  this.subTotal += item.count * item.price;
-                  this.int_shipping += item.count * item.int_shipping;
-                  this.shipping += item.count * item.shipping;
-                }
-                this.subTotal = this.subTotal.toFixed(2);
-                this.int_shipping = this.int_shipping.toFixed(2);
-                this.shipping = (this.shipping || 0).toFixed(2);
-              },
-              clear: function() {
-                this.items = {};
-                this.update();
-              },
-              checkOut: function(user) {
-                var order = {
-                  user_id: user.id,
-                  sub_total: this.subTotal,
-                  int_shipping: this.int_shipping,
-                  shipping: this.shipping,
-                  name: user.name,
-                  phone: user.phone,
-                  email: user.email,
-                  street: user.street,
-                  city: user.city,
-                  state: user.state,
-                  country: user.country,
-                  zip: user.zip,
-                  items: []
-                };
-                for (var id in this.items) {
-                  var item = this.items[id];
-                  order.items.push({
-                    item_id: item.id,
-                    price: item.price,
-                    count: item.count
-                  });
-                }
-                var cart = this;
-
-                var saveUserInfo = function() {
-                  if (user.isNew) {
-                    delete user.isNew;
-                    delete user.id;
-                  }
-                  return rpc.update_user(user).then(function(response) {
-                    var created = response.data.updated;
-                    if (!created) return order.user_id;
-                    return order.user_id = created.id;
-                  });
-                };
-                
-                var placeOrder = function() {
-                  return rpc.update_order(order).then(function(response) {
-                    if (parseInt(response.data.updated)) {
-                      cart.clear();
-                      $rootScope.$broadcast('reload-orders');
-                      document.querySelector('#toast0').open();
-                      return true;
-                    }
-                    return false;
-                  });
-                };
-                
-                return utils.requestOneByOne([saveUserInfo, placeOrder]);
-              }
-            };
+            scope.cart = new Cart({rpc: rpc, utils: utils, 
+                rootScope: $rootScope});
 
             rpc.get_user().then(function(user) {
               perm.user = user;
