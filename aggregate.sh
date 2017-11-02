b785ecd31d04b9882e1030570fdafbb49110328c
Xipan Xiao <xxp9@hotmail.com>
2017-11-01 21:23:32 -0700

    redirect to order.html by default

--- aggregate.sh
+++ aggregate.sh
@@ -5,7 +5,3 @@ rm -f order_admin.js
 for f in `cat order_admin.deps`; do cat $f >> order_admin.js; done;
 rm -f invoice_print.js
 for f in `cat invoice_print.deps`; do cat $f >> invoice_print.js; done;
-
-echo "RewriteEngine on" > .htaccess
-echo "RewriteCond %{HTTP:X-Forwarded-Proto} !https" >> .htaccess
-echo "RewriteRule .* https://%{HTTP_HOST}%{REQUEST_URI} [R,L]" >> .htaccess
